729654395ce28696e852b61a7420851b7a877851
Metamate <jakikob@gmail.com>
2022-05-24 15:22:56 +0200

    first commit

---
+++ src/boid.rs
@@ -0,0 +1,113 @@
+use std::ops::{Div, Sub};
+
+use bevy::prelude::*;
+use rand::{thread_rng, Rng};
+
+const PERCEPTION_RADIUS: f32 = 50.;
+
+#[derive(PartialEq, Copy, Clone, Component)]
+pub struct Boid {
+    pub position: Vec2,
+    pub velocity: Vec2,
+    pub acceleration: Vec2,
+    pub width: f32,
+    pub height: f32,
+    pub color: Color,
+    max_force: f32,
+    max_velocity: f32,
+    min_velocity: f32,
+}
+
+impl Boid {
+    pub fn new(pos_x: f32, pos_y: f32, width: f32, height: f32, color: (f32, f32, f32)) -> Self {
+        Self {
+            position: Vec2::new(pos_x, pos_y),
+            velocity: Vec2::new(random() - 0.5, random() - 0.5),
+            acceleration: Vec2::new(random() - 0.5, random() - 0.5),
+            max_force: 0.5,
+            max_velocity: 5.,
+            min_velocity: 1.5,
+            width,
+            height,
+            color: Color::rgb(color.0, color.1, color.2),
+        }
+    }
+
+    pub fn update(&mut self) {
+        self.acceleration = self.acceleration.clamp_length_max(self.max_force);
+        self.position += self.velocity;
+        self.velocity += self.acceleration;
+        self.velocity = self
+            .velocity
+            .clamp_length(self.min_velocity, self.max_velocity);
+        self.acceleration = Vec2::ZERO;
+    }
+
+    pub fn local_boids<'a>(&self, boids: &'a [Boid]) -> Vec<&'a Boid> {
+        boids
+            .iter()
+            .filter(|boid| {
+                *boid != self && self.position.distance(boid.position) < PERCEPTION_RADIUS
+            })
+            .collect()
+    }
+
+    pub fn contain(&mut self, left: f32, right: f32, bottom: f32, top: f32) {
+        if self.position.x > right {
+            self.position.x = left;
+        } else if self.position.x < left {
+            self.position.x = right;
+        }
+        if self.position.y > top {
+            self.position.y = bottom;
+        } else if self.position.y < bottom {
+            self.position.y = top;
+        }
+    }
+
+    pub fn alignment(&self, local_boids: &[&Boid]) -> Vec2 {
+        let len = local_boids.len();
+        if len == 0 {
+            return Vec2::ZERO;
+        }
+        local_boids
+            .iter()
+            .fold(Vec2::ZERO, |sum, boid| sum + boid.velocity)
+            .div(len as f32)
+            .sub(self.velocity)
+    }
+
+    pub fn cohesion(&self, local_boids: &[&Boid]) -> Vec2 {
+        let len = local_boids.len();
+        if len == 0 {
+            return Vec2::ZERO;
+        }
+        local_boids
+            .iter()
+            .fold(Vec2::ZERO, |sum, boid| sum + boid.position)
+            .div(len as f32)
+            .sub(self.position)
+    }
+
+    pub fn separation(&self, local_boids: &[&Boid]) -> Vec2 {
+        let len = local_boids.len();
+        if len == 0 {
+            return Vec2::ZERO;
+        }
+        local_boids.iter().fold(Vec2::ZERO, |sum, boid| {
+            let distance = self.position.distance(boid.position);
+            let inverse_magnitude = if distance != 0. { distance } else { f32::MIN };
+
+            sum - boid.position.sub(self.position).div(inverse_magnitude)
+        })
+    }
+}
+
+// HELPER FUNCTIONS
+pub fn random() -> f32 {
+    thread_rng().gen_range(0.0..=1.0)
+}
+
+pub fn random_range(min: f32, max: f32) -> f32 {
+    thread_rng().gen_range(min..=max)
+}
---
+++ src/main.rs
@@ -0,0 +1,97 @@
+mod boid;
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use boid::{random, random_range, Boid};
+
+const NO_BOIDS: u16 = 100;
+
+const ALIGNMENT: f32 = 1.;
+const COHESION: f32 = 0.05;
+const SEPARATION: f32 = 1.;
+
+pub const CLEAR: Color = Color::rgb(1., 1., 1.);
+
+pub struct BoidPlugin;
+
+impl Plugin for BoidPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GameTimer(Timer::from_seconds(0.01, true)))
+            .insert_resource(ClearColor(CLEAR))
+            .add_startup_system(setup)
+            .add_system(update_boids);
+    }
+}
+
+struct GameTimer(Timer);
+
+fn main() {
+    App::new()
+        .insert_resource(WindowDescriptor {
+            title: "Superboids".to_string(),
+            width: 1600.,
+            height: 900.,
+            ..default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(BoidPlugin)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    window: Res<WindowDescriptor>,
+) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    for _ in 0..NO_BOIDS {
+        let boid = Boid::new(
+            random_range(-window.width / 2., window.width / 2.),
+            random_range(-window.height / 2., window.height / 2.),
+            5.,
+            5.,
+            (random(), random(), random()),
+        );
+
+        commands
+            .spawn_bundle(MaterialMesh2dBundle {
+                mesh: meshes.add(Mesh::from(shape::Icosphere::default())).into(),
+                transform: Transform::from_xyz(boid.position.x, boid.position.y, 1.)
+                    .with_scale(Vec3::new(boid.width, boid.height, 1.)),
+                material: materials.add(ColorMaterial::from(boid.color)),
+                ..default()
+            })
+            .insert(boid);
+    }
+}
+
+fn update_boids(
+    time: Res<Time>,
+    window: Res<WindowDescriptor>,
+    mut timer: ResMut<GameTimer>,
+    mut query: Query<(&mut Boid, &mut Transform)>,
+) {
+    let boids: Vec<Boid> = query.iter().map(|(b, _)| *b).collect();
+
+    if timer.0.tick(time.delta()).just_finished() {
+        for (mut boid, mut transform) in query.iter_mut() {
+            let local_boids = boid.local_boids(&boids);
+            let alignment = boid.alignment(&local_boids);
+            let cohesion = boid.cohesion(&local_boids);
+            let separation = boid.separation(&local_boids);
+
+            boid.acceleration +=
+                alignment * ALIGNMENT + cohesion * COHESION + separation * SEPARATION;
+
+            boid.update();
+            boid.contain(
+                -window.width / 2.,
+                window.width / 2.,
+                -window.height / 2.,
+                window.height / 2.,
+            );
+            transform.translation.y = boid.position.y;
+            transform.translation.x = boid.position.x;
+        }
+    }
+}
